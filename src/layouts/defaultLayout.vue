dbe9ca585bcb11db4312395ea23d0f82651edca8
kromate <akpananthony33@gmail.com>
2022-05-04 12:27:44 +0100

    --- Auto-Git Commit ---

---
+++ src/layouts/defaultLayout.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="bg-dark min-h-screen overflow-hidden ">
+		<PageLoading />
+		<Alert />
+		<slot />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import PageLoading from '@/components/core/PageLoading.vue'
+import Alert from '@/components/core/Alert.vue'
+</script>
+
+<style scoped>
+
+</style>
+ 
---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<default-layout>
+		<div class="items-center justify-center flex">
+			Helloe
+		</div>
+	</default-layout>
+
+</template>
+
+<script setup lang="ts">
+import defaultLayout from '@/layouts/defaultLayout.vue'
+</script>
+
+<style scoped>
+
+</style>
